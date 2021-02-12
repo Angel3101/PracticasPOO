0860f64e257c71d82a0fffb89780a01f6e2ac11f
Angel3101 <angeljessbr31@gmail.com>
2021-02-12 13:30:25 -0600

    Actualizacion Programa Codigo 1 ITO Programacion Orientada a Objetos

---
+++ src/poo/app/Ejemplo1.java
@@ -0,0 +1,27 @@
+package poo.app;
+
+
+public class Ejemplo1 {
+	
+
+
+		
+	      static void run () {
+			
+			for(int i=0; i<=10; i++);
+			System.out.println("Hola Mundo");
+			
+			
+		}
+		
+		
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+	         run();
+	         
+		}
+		
+
+		
+
+}
